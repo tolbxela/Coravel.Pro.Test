d1190096649c76a76627caec23f0f31dcb85047a
Tolbxela <email@tolbxela.com>
2019-08-09 13:44:32 +0200

    "System.NullReferenceException" in app.UseCoravelPro();

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Coravel.Pro.EntityFramework;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoravelTest
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    "Server=(localdb)\\mssqllocaldb;Database=Coravel;Trusted_Connection=True;MultipleActiveResultSets=true"
+                );
+            }
+        }
+
+        public DbSet<CoravelJobHistory> Coravel_JobHistory { get; set; }
+        public DbSet<CoravelScheduledJob> Coravel_ScheduledJobs { get; set; }
+        public DbSet<CoravelScheduledJobHistory> Coravel_ScheduledJobHistory { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using Coravel.Pro;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoravelTest
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Coravel;Trusted_Connection=True;MultipleActiveResultSets=true")
+                .EnableSensitiveDataLogging(true)
+            );
+            services.AddCoravelPro(typeof(ApplicationDbContext));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCoravelPro();
+        }
+    }
+}
